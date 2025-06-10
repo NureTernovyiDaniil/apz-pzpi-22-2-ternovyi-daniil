7624ff91e1e58b417208a66302d50e93517edb2f
Daniil Ternovyi <daniil.ternovyi@nure.ua>
2025-06-10 23:08:14 +0300

    Lab 2 implementation

---
+++ Lab2/arkpz-pzpi-22-2-ternovyi-daniil-lab2/Controllers/OrderItemController.cs
@@ -0,0 +1,95 @@
+﻿using ChefMate_backend.Models;
+using ChefMate_backend.Repositories;
+using ChefMate_backend.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChefMate_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderItemController : ControllerBase
+    {
+        private readonly IOrderItemRepository _orderItemRepository;
+
+        public OrderItemController(IOrderItemRepository orderItemRepository)
+        {
+            _orderItemRepository = orderItemRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var orderItems = await _orderItemRepository.Retrieve();
+            return Ok(orderItems);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var orderItem = await _orderItemRepository.Retrieve(id);
+            return Ok(orderItem);
+        }
+
+        [HttpPost("post")]
+        public async Task<IActionResult> Post(OrderItemDto orderItem)
+        {
+            var result = await _orderItemRepository.Insert(orderItem);
+            if(result)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPost("post/list")]
+        public async Task<IActionResult> PostList(List<OrderItemDto> orderItem)
+        {
+            var result = await _orderItemRepository.Insert(orderItem);
+            if (result)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPut("update")]
+        public async Task<IActionResult> Update(OrderItemDto orderItem)
+        {
+            var result = await _orderItemRepository.Update(orderItem);
+            if (result)
+            {
+                return Ok(result);
+            }
+
+            return NotFound();
+        }
+
+        [HttpDelete("delete/{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var result = await _orderItemRepository.Delete(id);
+
+            if (result)
+            {
+                return Ok(result);
+            }
+
+            return NotFound();
+        }
+
+        [HttpDelete("delete")]
+        public async Task<IActionResult> Delete(OrderItemDto orderItem)
+        {
+            var result = await _orderItemRepository.Delete(orderItem);
+
+            if (result)
+            {
+                return Ok(result);
+            }
+
+            return NotFound();
+        }
+    }
+}
